466728b2c826fe4607f5be885078d1da595c58cc
Mohan <bvsmohan@gmail.com>
2024-06-12 16:53:53 +0530

    updated App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { PokemonProvider } from "./context/PokemonContext";
+import BattleContainer from "./components/BattleContainer/BattleContainer";
 
 function App() {
   return (
-    <></>
+    <PokemonProvider>
+      <BattleContainer />
+    </PokemonProvider>
   );
 }
 
